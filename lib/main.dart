b6f8de9e0c7ff292f42a06024e8912c32e560625
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2022-12-10 15:55:53 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LotteryPage(),
+    );
+  }
+}
+
+class LotteryPage extends StatefulWidget {
+  const LotteryPage({super.key});
+
+  @override
+  State<LotteryPage> createState() => _LotteryPageState();
+}
+
+class _LotteryPageState extends State<LotteryPage> {
+  @override
+  Widget build(BuildContext context) {
+    /// B:01~15
+    /// I:16~30
+    /// N:31~45
+    /// G:46~60
+    /// O:61~75
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BINGO'),
+      ),
+      body: Center(
+        child: GridView.count(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          crossAxisCount: 5,
+          children: [
+            for (var index = 0; index < 25; index++)
+              Center(child: Text('$index')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// BINGOをカードを生成する
+/// これは25個の数字の配列として表現できる
+List<int> generateBingoNumbers() {
+  /// まずは全部をシャッフルする
+  resourceB.shuffle();
+  resourceI.shuffle();
+  resourceN.shuffle();
+  resourceG.shuffle();
+  resource0.shuffle();
+
+  final bingoNumbers = [
+    ...resourceB.sublist(0, 5),
+    ...resourceI.sublist(0, 5),
+    ...resourceN.sublist(0, 5),
+    ...resourceG.sublist(0, 5),
+    ...resource0.sublist(0, 5),
+  ];
+}
+
+final resourceB = List.generate(15, (index) => index + 1);
+final resourceI = List.generate(15, (index) => index + 16);
+final resourceN = List.generate(15, (index) => index + 31);
+final resourceG = List.generate(15, (index) => index + 45);
+final resource0 = List.generate(15, (index) => index + 60);
